0425f93356189cccdec7041c53797ff9955e1748
pratham-kashyap <64340246+pratham-kashyap@users.noreply.github.com>
2024-03-02 16:20:31 +0530

    Day 2

--- 977. Squares of a Sorted Array.cpp
+++ 977. Squares of a Sorted Array.cpp
@@ -1,3 +1,4 @@
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
