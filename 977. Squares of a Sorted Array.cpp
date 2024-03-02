0790cc7ef830b5de8affd331bb7c93d51e1dd3f1
pratham-kashyap <64340246+pratham-kashyap@users.noreply.github.com>
2024-03-02 16:18:46 +0530

    Day 2

---
+++ 977. Squares of a Sorted Array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> neg;
+        vector<int> pos;
+        vector<int> ans;
+        int l1 = -1, l2 = -1;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]<0)
+            {
+                neg.push_back(nums[i]*nums[i]);
+                l1++;
+            }
+            else
+            {
+                pos.push_back(nums[i]*nums[i]);
+                l2++;
+            }
+        }
+        int i = 0;
+        while(l1>=0 && i<=l2)
+        {
+            if(neg[l1]<pos[i])
+                ans.push_back(neg[l1--]);
+            else
+                ans.push_back(pos[i++]);
+        }
+        while(l1>=0)
+        {
+            ans.push_back(neg[l1--]);
+        }
+        while(i<=l2)
+        {
+            ans.push_back(pos[i++]);
+        }
+        return ans;
+    }
+};
