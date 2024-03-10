85b60dfa13bd707377110be733ca999346ecb7e9
pratham-kashyap <64340246+pratham-kashyap@users.noreply.github.com>
2024-03-10 22:50:23 +0530

    Day 7

---
+++ 876. Middle of the Linked List.cpp
@@ -0,0 +1,28 @@
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* turtle = new ListNode();
+        ListNode* hare = new ListNode();
+        turtle = hare = head;
+        while(hare && hare->next)
+        {
+            turtle = turtle->next;
+            hare = hare->next->next;
+        }
+        return turtle;
+    }
+};
